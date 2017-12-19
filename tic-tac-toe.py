1a70b849dfdc3989b8aaec0135eb75cccf66f88e
Pritesh Ranjan <pranjan341@gmail.com>
2017-12-19 23:33:54 +0530

    fixed bug in draw logic

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,37 +3,31 @@
 # Author: Pritesh Ranjan
 
 import sys
-if sys.version_info >= (3, 0):
-  from tkinter import Tk, Button
-  from tkinter.font import Font
-  from tkinter import messagebox
-else:
-  from Tkinter import Tk, Button
-  from tkFont import Font
-  from Tkinter import messagebox
-
+from tkinter import Tk, Button
+from tkinter.font import Font
+from tkinter import messagebox
 
 #Global Variables
 computer = 'O'
 opponent = 'X'
 ActivePlayer = opponent
-p={} #what current player  selects
-turns=0
+p = {} #what current player  selects
+turns = 0 #number of tunrns
 
 root = Tk()
 root.resizable(width=False, height=False)
 root.title("The Undefeatable Tic Tac Toe Game")
-
 but = {}
+
 def Gui():
 	for x in range(3):
 		for y in range(3):
 			func = lambda x=x,y=y: ButtonClick(x,y)
-			but[x,y]= Button(root, text=' ', command=func, width=15, height=10)
+			but[x,y] = Button(root, text='|_|', command=func, width=15, height=10)
 			but[x,y].grid(row=y, column=x)
-			p[x,y]='.'
-
-	func=lambda : PlayAgain()
+			p[x,y] = '.'
+			
+	func = lambda : PlayAgain()
 	button = Button(root, text='Play Again', command=func)
 	button.grid(row=4, column=0, columnspan=1, sticky="WE")
 	
@@ -70,20 +64,19 @@ def ButtonClick(y,x):
 		if a:
 			turns=0
 			winner(ActivePlayer,b)
-		ActivePlayer=opponent
-
+		ActivePlayer= opponent
+		
+	#check for draw
 	if turns==9:
 		print('tie')
 		messagebox.showinfo(title="Whoaa !!", message="The game ends in a draw. Play again.")
+		turns=0
 		PlayAgain()
-
-
 
 def SetLayout(y, x, PlayerSymbol):
 	but[x,y]['text'] = PlayerSymbol
 	but[x,y]['state'] = 'disabled'
 	but[x,y]['background'] ='black'
-
 
 def winner(ActivePlayer, winning):
 	print('winner is {}'.format(ActivePlayer))
@@ -92,8 +85,6 @@ def winner(ActivePlayer, winning):
 	messagebox.showinfo(title="Congrats", message="Player {} has won".format(ActivePlayer))
 	PlayAgain()
 
-
-
 def CheckWinner(x,y):
 	print(p[x,y], x, y)
 	#column
@@ -101,25 +92,21 @@ def CheckWinner(x,y):
 		winning=[]
 		winning=((0,y),(1,y),(2,y))
 		return True, winning
-
 	#row
 	if (p[x,0] == p[x,1] == p[x,2] == ActivePlayer):
 		winning=[]
 		winning=((x,0),(x,1),(x,2))
 		return True, winning
-
     #diagonal1
 	if (x == y and p[0,0] == p[1,1] == p[2,2] == ActivePlayer):
 		winning=[]
 		winning=((0,0),(1,1),(2,2))
 		return True, winning
-
     #diagonal2
 	if (x + y == 2 and p[0,2] == p[1,1] == p[2,0] == ActivePlayer):
 		winning=[]
 		winning=((0,2),(1,1),(2,0))
 		return True, winning
-
 	return False, None  
 
 			
